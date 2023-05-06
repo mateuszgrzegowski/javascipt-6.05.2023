0dbfb4ff4b98dfffa060851e78f3455927af6eb6
mateuszgrzegowski <101980306+mateuszgrzegowski@users.noreply.github.com>
2023-05-06 15:37:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("hello world");
+
+//operatory arytmetyczne
+
+var x = 2;
+
+console.log(x + x);
+console.log(5 - 2);
+console.log(5 * x);
+console.log(10 / x);
+console.log( 5 % x);
+
+console.log("5" + 2);
+console.log("5" * 2);
+console.log("5" - 2);
+console.log("5" / 2);
+console.log("5" % 2);
+console.log("abc" * 2);
+
+console.log(2 + 2 * 2);
+console.log((2+2) * 2);
+
+console.log ("x = " + x);
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 2;
+x %= 2;
+console.log ("x = " + x);
+console.log (x);
+console.log (x);
+console.log (x);
+console.log (x);
+
+x++;
+console.log(x);
+x--;
+console.log(x);
---
+++ script2.js
@@ -0,0 +1,28 @@
+//operatory porownania
+
+var x = 5 == 5;
+console.log(x); 
+console.log(typeof(x));
+
+console.log("----------------");
+console.log( 5 == 5);
+console.log ( 5 == 0);
+console.log ( 5 === 5);
+
+console.log( 5 != 5);
+console.log( 5 !==5 ); 
+
+console.log( 5 >= 5);
+console.log(5 > 5);
+
+console.log(5 < 5);
+console.log(5 <= 5);
+
+//operatory logiczne
+console.log("--------------------");
+
+console.log(true || true);
+console.log(true && true);
+console.log(!5 >  0);
+
+console.log((true || true) && !false);
